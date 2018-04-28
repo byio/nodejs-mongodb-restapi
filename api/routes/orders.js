127259663dc01105abd548da82b956dac2141ad7
byio <ben@benyang.io>
2018-04-28 23:20:23 +0800

    create route to handle GET requests to /orders/orderId

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,6 +8,12 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: `Handling GET requests to /orders/${req.params.orderId}`
+  })
+});
+
 router.post('/', (req, res, next) => {
   res.status(201).json({
     message: 'Handling POST requests to /orders'
