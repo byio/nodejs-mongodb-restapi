f88e13736b31c63f07d8d20c0c303c66f4125190
byio <ben@benyang.io>
2018-04-28 23:07:57 +0800

    create route to handle GET requests to /orders

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 
 // import routes
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
 // routes middleware
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
