4ecadd964f9cd16c598d6cc53c379d3e8926f6f1
byio <ben@benyang.io>
2018-04-29 20:17:00 +0800

    parse json body of POST requests to /products

--- api/routes/products.js
+++ api/routes/products.js
@@ -23,8 +23,13 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+  const createdProduct = {
+    name: req.body.name,
+    price: req.body.price
+  };
   res.status(201).json({
-    message: 'Handling POST requests to /products'
+    message: 'Handling POST requests to /products',
+    createdProduct
   });
 });
 
