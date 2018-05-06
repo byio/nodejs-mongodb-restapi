25c758d234cfcbfe3f0d95ca62d370d68fb2e8cc
byio <ben@benyang.io>
2018-05-06 22:28:52 +0800

    only save order when the productId is a valid id

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 // import order model
 const Order = require('../models/order');
+const Product = require('../models/product');
 
 const router = express.Router();
 
@@ -41,33 +42,38 @@ router.get('/:orderId', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const newOrder = new Order({
-    _id: mongoose.Types.ObjectId(),
-    product: req.body.productId,
-    quantity: req.body.quantity
-  });
-  newOrder.save()
-       .then(result => {
-         const { _id, product, quantity } = result;
-         const jsonResponse = {
-           message: 'New order created!',
-           createdOrder: {
-             _id, product, quantity
-           },
-           requests: [
-             {
-               type: 'GET',
-               url: `http://localhost:4000/orders/${_id}`,
-               description: 'get information about individual orders'
-             }
-           ]
-         };
-         res.status(201).json(jsonResponse);
-       })
-       .catch(error => {
-         console.log(error);
-         res.status(500).json({ error });
-       });
+  Product.findById(req.body.productId)
+         .then(product => {
+           const newOrder = new Order({
+             _id: mongoose.Types.ObjectId(),
+             product: req.body.productId,
+             quantity: req.body.quantity
+           });
+           return newOrder.save();
+         })
+         .then(result => {
+            const { _id, product, quantity } = result;
+            const jsonResponse = {
+              message: 'New order created!',
+              createdOrder: {
+                _id, product, quantity
+              },
+              requests: [
+                {
+                  type: 'GET',
+                  url: `http://localhost:4000/orders/${_id}`,
+                  description: 'get information about individual orders'
+                }
+              ]
+            };
+            res.status(201).json(jsonResponse);
+          })
+          .catch(error => {
+            res.status(500).json({
+              message: 'Cannot add invalid product',
+              error
+            });
+          });
 });
 
 router.delete('/:orderId', (req, res, next) => {
