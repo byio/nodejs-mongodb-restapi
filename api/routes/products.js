443112b40334d5f4b2b7885eb2165384e03b67e5
byio <ben@benyang.io>
2018-05-09 15:23:56 +0800

    throw an error for uploading files that do not have mimetypes that are jpeg or png

--- api/routes/products.js
+++ api/routes/products.js
@@ -20,7 +20,7 @@ const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
-    cb(null, false);
+    cb(new Error('Invalid file extension.'), false);
   }
 };
 const upload = multer({
