1c840b937a4195257ddfb12916fce3162a34f5a1
byio <ben@benyang.io>
2018-05-10 08:52:36 +0800

    create new user with hashed password using bcrypt, and save new user to database

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
@@ -8,6 +9,32 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'GET /users'
+  });
+});
+
+router.post('/signup', (req, res, next) => {
+  const { username, email, password } = req.body;
+  bcrypt.hash(password, 10, (error, hash) => {
+    if (error) {
+      return res.status(500).json({ error });
+    } else {
+      const newUser = new User({
+        _id: mongoose.Types.ObjectId(),
+        username,
+        email,
+        password: hash
+      });
+      newUser.save()
+             .then(result => {
+               res.status(201).json({
+                 message: 'New user created.',
+                 result
+               });
+             })
+             .catch(error => {
+               res.status(500).json({ error });
+             });
+    }
   });
 });
 
