a9bcbf2566795315d57a9cb930557a5d0d172867
byio <ben@benyang.io>
2018-05-14 19:05:39 +0800

    add controller to handle the logic behind POST requests to /orders/:orderId

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -16,45 +16,7 @@ router.get('/', checkAuth, OrderController.orders_get_all);
 
 router.get('/:orderId', checkAuth, OrderController.orders_get_one);
 
-router.post('/', checkAuth, (req, res, next) => {
-  Product.findById(req.body.productId)
-         .then(product => {
-           if (!product) {
-             return res.status(404).json({
-               message: 'Product not found.'
-             });
-           }
-           const newOrder = new Order({
-             _id: mongoose.Types.ObjectId(),
-             product: req.body.productId,
-             quantity: req.body.quantity
-           });
-           return newOrder.save();
-         })
-         .then(result => {
-            const { _id, product, quantity } = result;
-            const jsonResponse = {
-              message: 'New order created!',
-              createdOrder: {
-                _id, product, quantity
-              },
-              requests: [
-                {
-                  type: 'GET',
-                  url: `http://localhost:4000/orders/${_id}`,
-                  description: 'get information about individual orders'
-                }
-              ]
-            };
-            res.status(201).json(jsonResponse);
-          })
-          .catch(error => {
-            res.status(500).json({
-              message: 'Cannot add invalid product',
-              error
-            });
-          });
-});
+router.post('/', checkAuth, OrderController.orders_create_one);
 
 router.delete('/:orderId', checkAuth, (req, res, next) => {
   Order.remove({ _id: req.params.orderId })
