f60256427fa916371286367a00e23c2d1110327f
byio <ben@benyang.io>
2018-05-14 15:26:54 +0800

    when handling DELETE requests to /products/:productId, check that product exists before deleting the product

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -140,25 +140,35 @@ exports.products_update_one = (req, res, next) => {
 };
 
 exports.products_delete_one = (req, res, next) => {
-  Product.remove({ _id: req.params.productId })
+  const { productId } = req.params;
+  Product.findById(productId)
          .exec()
-         .then(result => {
-           // console.log(result);
-           const jsonResponse = {
-             message: `Product with ID ${req.params.productId} has been deleted successfully!`,
-             requests: [
-               {
-                 type: 'POST',
-                 url: 'http://localhost:4000/products',
-                 body: { name: 'String', price: 'Number' },
-                 description: 'create a new product'
-               }
-             ]
-           };
-           res.status(200).json(jsonResponse);
+         .then(product => {
+           if (!product) {
+             return res.status(404).json({
+               message: 'Product not found.'
+             });
+           }
+           Product.remove({ _id: req.params.productId })
+                  .exec()
+                  .then(result => {
+                    // console.log(result);
+                    const jsonResponse = {
+                      message: `Product with ID ${req.params.productId} has been deleted successfully!`,
+                      requests: [
+                        {
+                          type: 'POST',
+                          url: 'http://localhost:4000/products',
+                          body: { name: 'String', price: 'Number' },
+                          description: 'create a new product'
+                        }
+                      ]
+                    };
+                    res.status(200).json(jsonResponse);
+                  });
          })
          .catch(error => {
-           console.log(error);
            res.status(500).json({ error });
          });
+
 };
