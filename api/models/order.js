0338dcf04a9c64137e0fb9cb5f1577e78696e997
byio <ben@benyang.io>
2018-05-16 10:23:16 +0800

    add user field to Order schema

--- api/models/order.js
+++ api/models/order.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   product: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Product',
